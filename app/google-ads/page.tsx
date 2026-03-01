0383e84572bca45e97d111439ca2ee3fe0911169
mf-fatima <mf8632744@gmail.com>
2026-03-01 14:52:50 +0500

    Completed Google Ads page

--- app/google-ads/page.tsx
+++ app/google-ads/page.tsx
@@ -1,8 +1,12 @@
-import Hero from "./components/hero";
-import DidYouKnow from "./components/didYouKnow";
-import Trust from "./components/trust";
-import OurApproach from "./components/ourApproach";
-import ExpertiseBanner from "./components/expertise";
+import Hero from "../component/google-ads-page/hero";
+import DidYouKnow from "../component/google-ads-page/didYouKnow";
+import Trust from "../component/google-ads-page/trust";
+import OurApproach from "../component/google-ads-page/ourApproach";
+import ExpertiseBanner from "../component/google-ads-page/expertise";
+import StrategyCallSection from "../component/google-ads-page/strategy-call";
+import FAQSection from "../component/google-ads-page/FAQ-section";
+import  TestimonialsSection  from "../component/google-ads-page/testimonials";
+import Contact from "../component/google-ads-page/contact";
 export default function GoogleAdsPage() {
   return (
     <div className="bg-black text-white">
@@ -11,6 +15,10 @@ export default function GoogleAdsPage() {
       <Trust />
       <OurApproach />
      < ExpertiseBanner/>
+     <StrategyCallSection/>
+     <FAQSection/>
+     <TestimonialsSection/>
+     <Contact/>
     </div>
   );
 }
